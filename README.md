eeafada3017b97f8ed5f42dcf8d1a19e5130fea2
mahmudul <mahmudul@businessaccelerate.com.bd>
2023-04-27 08:50:02 +0600

    TS Day 1

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 4. How to watch Typescript compiler (tsc -w).
 5. How to convert Typescript to Javascript.
 
+# Array
+1. Same type of array property ["apple", "orange", "banana"]; only string type will be accept.
+2. Mixed type of array property ["apple", 45, false]; only string, number and boolen type will be accept.
+
+# Object
+1. Learn object work like a schema
--- src/script.ts
+++ src/script.ts
@@ -1,8 +1,35 @@
-const country = "valobasi Bangladesh";
-
-const test=()=>{
-    console.log(country);
-};
-test()
+// const country = "valobasi Bangladesh";
 
 
+// const test=()=>{
+//     console.log(country);
+// };
+// test()
+
+// array
+
+let fruits = ["apple", "banana", "orange"];
+
+fruits.push("lichi") // it will be acceptable. because the array type is string
+
+fruits.push(34) // it give an error, because the assing value not a string
+
+let mixed  = ["apple", 43, true];
+
+mixed.push(90) // true beacuse array type is mixed and inside array there is a number varible
+
+
+
+// Object
+
+let developer ={
+    name: "Hasibul",
+    age:24,
+    jobStatus: true
+}
+
+
+developer.name =" Hasan" // true because inside object property name is a string type
+developer.name = 67 // flase because inside object property name is not a number type
+
+// now new peroperty adding on object, beacuse typescript take a define object like a schema
